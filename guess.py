88577450556e4782f9234df681c8ab86a177b55d
Ess James <111387393+essjames@users.noreply.github.com>
2023-06-12 22:08:49 +0100

    Update guess.py
    updated assignment operator and add a couple more f strings

--- guess.py
+++ guess.py
@@ -4,17 +4,17 @@ player = input("What is your name? ")
 amount_of_guesses = 0
 answer = random.randint(0,15)
 
-while amount_of_guesses < 5:
+while amount_of_guesses <= 5:
     greeting = (f"Hi {player}! I am thinking of a number between 1-15...")
     guess = int(input("You have five chances to guess what I am thinking, pick a number between 1-15:  "))
-    amount_of_guesses =+ 1
+    amount_of_guesses += 1
     if guess == answer:
         print(f"{player} you have won!!! You guessed {guess} & The Computer was thinking of {answer}")
         break
     elif guess != answer and amount_of_guesses == 5:
-        print("Oops you have ran out of Guesses :(! Better look next time!")
+        print(f"Oops you have ran out of Guesses {player} :(! Better look next time! The Computer was thinking of {answer}")
         break
-    elif guess != answer:
-        print("Opps! Guess Again!")
+    elif guess != answer and amount_of_guesses < 5:
+        print(f"Opps! Guess Again {name}!")
 else:
-    print(f"You did not guess correctly, the number was {answer}")
+    print(f"You did not guess correctly {player}, the number was {answer}")
